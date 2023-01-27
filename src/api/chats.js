3c149811f5dedc71cdce24d48e299ea4ff6c86ae
刘骏一 <liujunyi@The-Rose-Air.local>
2023-01-27 14:41:12 -0500

    Translated two functions and declared types for use This version was able to get through npm test, which signals that the first change was doable

--- src/api/chats.js
+++ src/api/chats.js
@@ -1,4 +1,5 @@
-'use strict';
+"use strict";
+// 'use strict';
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,25 +9,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const validator = require('validator');
-const user = require('../user');
-const meta = require('../meta');
-const messaging = require('../messaging');
-const plugins = require('../plugins');
-// const websockets = require('../socket.io');
-const socketHelpers = require('../socket.io/helpers');
-const chatsAPI = module.exports;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.kick = exports.invite = exports.users = exports.rename = exports.post = exports.create = void 0;
+// const validator = require('validator');
+// const user = require('../user');
+// const meta = require('../meta');
+// const messaging = require('../messaging');
+// const plugins = require('../plugins');
+// // const websockets = require('../socket.io');
+// const socketHelpers = require('../socket.io/helpers');
+const validator_1 = __importDefault(require("validator"));
+const user_1 = __importDefault(require("../user"));
+const meta_1 = __importDefault(require("../meta"));
+const messaging_1 = __importDefault(require("../messaging"));
+const plugins_1 = __importDefault(require("../plugins"));
+const helpers_1 = __importDefault(require("../socket.io/helpers"));
 function rateLimitExceeded(caller) {
     const session = caller.request ? caller.request.session : caller.session; // socket vs req
     const now = Date.now();
     session.lastChatMessageTime = session.lastChatMessageTime || 0;
-    if (now - session.lastChatMessageTime < meta.config.chatMessageDelay) {
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    if (now - session.lastChatMessageTime < meta_1.default.config.chatMessageDelay) {
         return true;
     }
     session.lastChatMessageTime = now;
     return false;
 }
-chatsAPI.create = function (caller, data) {
+// export async function create(caller, data) {
+//     if (rateLimitExceeded(caller)) {
+//         throw new Error('[[error:too-many-messages]]');
+//     }
+//     if (!data.uids || !Array.isArray(data.uids)) {
+//         throw new Error(`[[error:wrong-parameter-type, uids, ${typeof data.uids}, Array]]`);
+//     }
+//     await Promise.all(data.uids.map(async uid => messaging.canMessageUser(caller.uid, uid)));
+//     const roomId = await messaging.newRoom(caller.uid, data.uids);
+//     return await messaging.getRoomData(roomId);
+// };
+function create(caller, data) {
     return __awaiter(this, void 0, void 0, function* () {
         if (rateLimitExceeded(caller)) {
             throw new Error('[[error:too-many-messages]]');
@@ -34,77 +58,104 @@ chatsAPI.create = function (caller, data) {
         if (!data.uids || !Array.isArray(data.uids)) {
             throw new Error(`[[error:wrong-parameter-type, uids, ${typeof data.uids}, Array]]`);
         }
-        yield Promise.all(data.uids.map((uid) => __awaiter(this, void 0, void 0, function* () { return messaging.canMessageUser(caller.uid, uid); })));
-        const roomId = yield messaging.newRoom(caller.uid, data.uids);
-        return yield messaging.getRoomData(roomId);
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        yield Promise.all(data.uids.map((uid) => __awaiter(this, void 0, void 0, function* () { return messaging_1.default.canMessageUser(caller.uid, uid); })));
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        const roomId = yield messaging_1.default.newRoom(caller.uid, data.uids);
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call,
+        return yield messaging_1.default.getRoomData(roomId);
     });
-};
-chatsAPI.post = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
-    if (rateLimitExceeded(caller)) {
-        throw new Error('[[error:too-many-messages]]');
-    }
-    ({ data } = yield plugins.hooks.fire('filter:messaging.send', {
-        data,
-        uid: caller.uid,
-    }));
-    yield messaging.canMessageRoom(caller.uid, data.roomId);
-    const message = yield messaging.sendMessage({
-        uid: caller.uid,
-        roomId: data.roomId,
-        content: data.message,
-        timestamp: Date.now(),
-        ip: caller.ip,
+}
+exports.create = create;
+function post(caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (rateLimitExceeded(caller)) {
+            throw new Error('[[error:too-many-messages]]');
+        }
+        ({ data } = yield plugins_1.default.hooks.fire('filter:messaging.send', {
+            data,
+            uid: caller.uid,
+        }));
+        yield messaging_1.default.canMessageRoom(caller.uid, data.roomId);
+        const message = yield messaging_1.default.sendMessage({
+            uid: caller.uid,
+            roomId: data.roomId,
+            content: data.message,
+            timestamp: Date.now(),
+            ip: caller.ip,
+        });
+        messaging_1.default.notifyUsersInRoom(caller.uid, data.roomId, message);
+        user_1.default.updateOnlineUsers(caller.uid);
+        return message;
     });
-    messaging.notifyUsersInRoom(caller.uid, data.roomId, message);
-    user.updateOnlineUsers(caller.uid);
-    return message;
-});
-chatsAPI.rename = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
-    yield messaging.renameRoom(caller.uid, data.roomId, data.name);
-    const uids = yield messaging.getUidsInRoom(data.roomId, 0, -1);
-    const eventData = { roomId: data.roomId, newName: validator.escape(String(data.name)) };
-    socketHelpers.emitToUids('event:chats.roomRename', eventData, uids);
-    return messaging.loadRoom(caller.uid, {
-        roomId: data.roomId,
+}
+exports.post = post;
+;
+function rename(caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield messaging_1.default.renameRoom(caller.uid, data.roomId, data.name);
+        const uids = yield messaging_1.default.getUidsInRoom(data.roomId, 0, -1);
+        const eventData = { roomId: data.roomId, newName: validator_1.default.escape(String(data.name)) };
+        helpers_1.default.emitToUids('event:chats.roomRename', eventData, uids);
+        return messaging_1.default.loadRoom(caller.uid, {
+            roomId: data.roomId,
+        });
     });
-});
-chatsAPI.users = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
-    const [isOwner, users] = yield Promise.all([
-        messaging.isRoomOwner(caller.uid, data.roomId),
-        messaging.getUsersInRoom(data.roomId, 0, -1),
-    ]);
-    users.forEach((user) => {
-        user.canKick = (parseInt(user.uid, 10) !== parseInt(caller.uid, 10)) && isOwner;
+}
+exports.rename = rename;
+;
+function users(caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const [isOwner, users] = yield Promise.all([
+            messaging_1.default.isRoomOwner(caller.uid, data.roomId),
+            messaging_1.default.getUsersInRoom(data.roomId, 0, -1),
+        ]);
+        users.forEach((user) => {
+            user.canKick = (parseInt(user.uid, 10) !== parseInt(caller.uid, 10)) && isOwner;
+        });
+        return { users };
     });
-    return { users };
-});
-chatsAPI.invite = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
-    const userCount = yield messaging.getUserCountInRoom(data.roomId);
-    const maxUsers = meta.config.maximumUsersInChatRoom;
-    if (maxUsers && userCount >= maxUsers) {
-        throw new Error('[[error:cant-add-more-users-to-chat-room]]');
-    }
-    const uidsExist = yield user.exists(data.uids);
-    if (!uidsExist.every(Boolean)) {
-        throw new Error('[[error:no-user]]');
-    }
-    yield Promise.all(data.uids.map((uid) => __awaiter(void 0, void 0, void 0, function* () { return messaging.canMessageUser(caller.uid, uid); })));
-    yield messaging.addUsersToRoom(caller.uid, data.uids, data.roomId);
-    delete data.uids;
-    return chatsAPI.users(caller, data);
-});
-chatsAPI.kick = (caller, data) => __awaiter(void 0, void 0, void 0, function* () {
-    const uidsExist = yield user.exists(data.uids);
-    if (!uidsExist.every(Boolean)) {
-        throw new Error('[[error:no-user]]');
-    }
-    // Additional checks if kicking vs leaving
-    if (data.uids.length === 1 && parseInt(data.uids[0], 10) === caller.uid) {
-        yield messaging.leaveRoom([caller.uid], data.roomId);
-    }
-    else {
-        yield messaging.removeUsersFromRoom(caller.uid, data.uids, data.roomId);
-    }
-    delete data.uids;
-    return chatsAPI.users(caller, data);
-});
+}
+exports.users = users;
+;
+function invite(caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const userCount = yield messaging_1.default.getUserCountInRoom(data.roomId);
+        const maxUsers = meta_1.default.config.maximumUsersInChatRoom;
+        if (maxUsers && userCount >= maxUsers) {
+            throw new Error('[[error:cant-add-more-users-to-chat-room]]');
+        }
+        const uidsExist = yield user_1.default.exists(data.uids);
+        if (!uidsExist.every(Boolean)) {
+            throw new Error('[[error:no-user]]');
+        }
+        yield Promise.all(data.uids.map((uid) => __awaiter(this, void 0, void 0, function* () { return messaging_1.default.canMessageUser(caller.uid, uid); })));
+        yield messaging_1.default.addUsersToRoom(caller.uid, data.uids, data.roomId);
+        delete data.uids;
+        return users(caller, data);
+    });
+}
+exports.invite = invite;
+;
+function kick(caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const uidsExist = yield user_1.default.exists(data.uids);
+        if (!uidsExist.every(Boolean)) {
+            throw new Error('[[error:no-user]]');
+        }
+        // Additional checks if kicking vs leaving
+        if (data.uids.length === 1 && parseInt(data.uids[0], 10) === caller.uid) {
+            yield messaging_1.default.leaveRoom([caller.uid], data.roomId);
+        }
+        else {
+            yield messaging_1.default.removeUsersFromRoom(caller.uid, data.uids, data.roomId);
+        }
+        delete data.uids;
+        return users(caller, data);
+    });
+}
+exports.kick = kick;
+;
--- src/api/chats.ts
+++ src/api/chats.ts
@@ -1,21 +1,90 @@
-'use strict';
+// 'use strict';
 
-const validator = require('validator');
+// const validator = require('validator');
 
-const user = require('../user');
-const meta = require('../meta');
-const messaging = require('../messaging');
-const plugins = require('../plugins');
+// const user = require('../user');
+// const meta = require('../meta');
+// const messaging = require('../messaging');
+// const plugins = require('../plugins');
 
-// const websockets = require('../socket.io');
-const socketHelpers = require('../socket.io/helpers');
+// // const websockets = require('../socket.io');
+// const socketHelpers = require('../socket.io/helpers');
 
-const chatsAPI = module.exports;
 
-function rateLimitExceeded(caller) {
-    const session = caller.request ? caller.request.session : caller.session; // socket vs req
-    const now = Date.now();
+import validator from 'validator';
+
+import user from '../user';
+import meta from '../meta';
+import messaging from '../messaging';
+import plugins from '../plugins';
+
+import socketHelpers from '../socket.io/helpers';
+
+
+
+
+
+
+
+
+
+// const chatsAPI = module.exports;
+
+// function rateLimitExceeded(caller) {
+//     const session = caller.request ? caller.request.session : caller.session; // socket vs req
+//     const now = Date.now();
+//     session.lastChatMessageTime = session.lastChatMessageTime || 0;
+//     if (now - session.lastChatMessageTime < meta.config.chatMessageDelay) {
+//         return true;
+//     }
+//     session.lastChatMessageTime = now;
+//     return false;
+// }
+
+
+
+
+type Session = {
+    lastChatMessageTime : number
+}
+
+type Data = {
+    uids : string[]
+    roomId: number
+    message: string
+    name : string
+}
+
+type Request = {
+    session : Session
+}
+
+type Caller = {
+    request : Request,
+    session : Session
+    uid : string
+    ip : string
+}
+
+
+
+type EventData = {
+    roomId : number,
+    newName : string
+}
+
+type User = {
+    uid : string,
+    canKick : boolean
+}
+
+function rateLimitExceeded(caller : Caller) : boolean {
+    const session : Session = caller.request ? caller.request.session : caller.session; // socket vs req
+    const now : number = Date.now();
     session.lastChatMessageTime = session.lastChatMessageTime || 0;
+
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     if (now - session.lastChatMessageTime < meta.config.chatMessageDelay) {
         return true;
     }
@@ -23,7 +92,37 @@ function rateLimitExceeded(caller) {
     return false;
 }
 
-chatsAPI.create = async function (caller, data) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export async function create(caller, data) {
+//     if (rateLimitExceeded(caller)) {
+//         throw new Error('[[error:too-many-messages]]');
+//     }
+
+//     if (!data.uids || !Array.isArray(data.uids)) {
+//         throw new Error(`[[error:wrong-parameter-type, uids, ${typeof data.uids}, Array]]`);
+//     }
+
+//     await Promise.all(data.uids.map(async uid => messaging.canMessageUser(caller.uid, uid)));
+//     const roomId = await messaging.newRoom(caller.uid, data.uids);
+
+//     return await messaging.getRoomData(roomId);
+// };
+export async function create(caller : Caller, data : Data) {
     if (rateLimitExceeded(caller)) {
         throw new Error('[[error:too-many-messages]]');
     }
@@ -32,13 +131,29 @@ chatsAPI.create = async function (caller, data) {
         throw new Error(`[[error:wrong-parameter-type, uids, ${typeof data.uids}, Array]]`);
     }
 
+
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     await Promise.all(data.uids.map(async uid => messaging.canMessageUser(caller.uid, uid)));
-    const roomId = await messaging.newRoom(caller.uid, data.uids);
 
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const roomId : number = await messaging.newRoom(caller.uid, data.uids) as number;
+
+    // The next line calls a function in a module that has not been updated to TS yet
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call,
     return await messaging.getRoomData(roomId);
-};
+}
 
-chatsAPI.post = async (caller, data) => {
+
+
+
+
+
+
+
+
+export async function post(caller, data) {
     if (rateLimitExceeded(caller)) {
         throw new Error('[[error:too-many-messages]]');
     }
@@ -62,7 +177,7 @@ chatsAPI.post = async (caller, data) => {
     return message;
 };
 
-chatsAPI.rename = async (caller, data) => {
+export async function rename(caller, data) {
     await messaging.renameRoom(caller.uid, data.roomId, data.name);
     const uids = await messaging.getUidsInRoom(data.roomId, 0, -1);
     const eventData = { roomId: data.roomId, newName: validator.escape(String(data.name)) };
@@ -73,7 +188,7 @@ chatsAPI.rename = async (caller, data) => {
     });
 };
 
-chatsAPI.users = async (caller, data) => {
+export async function users(caller, data) {
     const [isOwner, users] = await Promise.all([
         messaging.isRoomOwner(caller.uid, data.roomId),
         messaging.getUsersInRoom(data.roomId, 0, -1),
@@ -84,7 +199,7 @@ chatsAPI.users = async (caller, data) => {
     return { users };
 };
 
-chatsAPI.invite = async (caller, data) => {
+export async function invite(caller, data) {
     const userCount = await messaging.getUserCountInRoom(data.roomId);
     const maxUsers = meta.config.maximumUsersInChatRoom;
     if (maxUsers && userCount >= maxUsers) {
@@ -99,10 +214,10 @@ chatsAPI.invite = async (caller, data) => {
     await messaging.addUsersToRoom(caller.uid, data.uids, data.roomId);
 
     delete data.uids;
-    return chatsAPI.users(caller, data);
+    return users(caller, data);
 };
 
-chatsAPI.kick = async (caller, data) => {
+export async function kick(caller, data) {
     const uidsExist = await user.exists(data.uids);
     if (!uidsExist.every(Boolean)) {
         throw new Error('[[error:no-user]]');
@@ -116,5 +231,5 @@ chatsAPI.kick = async (caller, data) => {
     }
 
     delete data.uids;
-    return chatsAPI.users(caller, data);
+    return users(caller, data);
 };
